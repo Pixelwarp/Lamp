9ba47f2b00dd5c2a2b0ad354d3799f77a36e3772
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-03-07 22:45:46 +0300

    Add an example plugin

---
+++ src/main/java/org/example/ExamplePlugin.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExamplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
